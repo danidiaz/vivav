bee9fb4223755f994b384d3ee57d473871b0e1e0
Daniel Diaz <diaz.carrete@facebook.com>
2016-09-14 12:37:11 +0200

    socket listen

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,25 @@
+{-# language TypeApplications #-}
 module Main where
 
 import Control.Monad
 import Control.Applicative
+import Control.Lens
+import Data.Aeson
 import Network
 import Pipes
-import qualified  Pipes.Aeson
+import qualified Pipes.ByteString
+import qualified Pipes.Aeson.Unchecked
+import System.IO
 
 main :: IO ()
-main = return ()
+main = do
+    let portID = PortNumber 7777
+    socket <- listenOn portID
+    putStrLn "Listening..."
+    forever $ do (handle,_,_) <- accept socket
+                 putStrLn "Connection accepted."
+                 hSetBuffering handle NoBuffering
+                 let jsonStream = view Pipes.Aeson.Unchecked.decoded
+                                       (Pipes.ByteString.fromHandle handle)
+                 runEffect $ for jsonStream $ lift . print @Value
+                 putStrLn "wee!"
